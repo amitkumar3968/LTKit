3855a4af1672b7f7d8f7e4be9cffbe861f9d553b
LucasTizma <lucas@tiz.ma>
2012-04-23 12:48:37 -0700

    Replace arc4random() with arc4random_uniform()

---
+++ LTKit/Utilities/LTKTesting.h
@@ -0,0 +1,14 @@
+//
+//  LTKTesting.h
+//  LTKit
+//
+//  Created by Michael Shannon Potter on 4/19/12.
+//  Copyright (c) 2012 LucasTizma. All rights reserved.
+//
+
+#ifndef LTKit_LTKTesting_h
+#define LTKit_LTKTesting_h
+
+
+
+#endif
